f13921ced9a06c152cf909e884394a3541412860
engineers_bro <kartikchauhan336@gmail.com>
2025-02-02 18:02:12 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -5,38 +5,49 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/usersRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
-import postRoutes from "./routes/postRoutes.js"
-import courseRoutes from "./routes/courseRoutes.js"
-import categoryRoutes from "./routes/categoryRoutes.js"
-import queryRoutes from "./routes/queryRoutes.js"
+import postRoutes from "./routes/postRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import queryRoutes from "./routes/queryRoutes.js";
+import path from "path";
 const app = express();
 dotenv.config();
+
+import { fileURLToPath } from "url";
+
+// Derive __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const corsOptions = {
+  origin: "https://jobgram.ranakartikchauhan.in",
+  credentials: true,
+};
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, {"useNewUrlParser": true, "useUnifiedTopology": true});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.connection.on("error", (error) => console.error(error));
 mongoose.connection.on("open", () => console.log("Connected to database"));
 
-
-
-
-
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/post",postRoutes);
-app.use("/api/course",courseRoutes);
-app.use("/api/category",categoryRoutes);
-app.use("/api/query",queryRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/course", courseRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/query", queryRoutes);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Fallback for all other routes to serve the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
-
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
