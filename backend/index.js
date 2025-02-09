5fbd6bed7dacf842b7c4ef452e0fb69988e9422e
ranakartikchauhan <kartikchauhan336@gmail.com>
2025-02-09 11:25:29 +0530

    comment index

--- backend/index.js
+++ backend/index.js
@@ -64,9 +64,9 @@ init(server);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Fallback for all other routes to serve the index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
 
 // Clear blocked IPs older than 24 hours
 cron.schedule("0 0 * * *", async () => {
