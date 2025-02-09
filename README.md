8e788ef59a68b7ce00df7b55f8c1a7e823382405
Sinesh <sinesh.kumar@zscaler.com>
2025-02-09 14:57:55 +0530

    resolve the port and package isses

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # MenteesDev
+
+# MenteesDev
+
+Clone the repo
+git clone https://github.com/CodeMentees/MenteesDev.git
+cd  cd MenteesDev
+Windows 
+# install node and npm 
+- winget search npm
+- winget install OpenJS.NodeJS.LTS (22.0 for me)
+- Allow yes, when sys promt appears
+- close the terminal and open a new one
+- node --version # v22.13.1
+- npm --version # 10.9.2
+# Install Vercel
+- npm install -g vercel
+- vercel --version # Vercel CLI 41.0.2
+
+# Deploy with vercel 
+vercel login
+enter --> make sure you are logged in with the same github account in your default browser. 
+
+-- la la la next to be continued. 
--- backend/index.js
+++ backend/index.js
@@ -57,16 +57,16 @@ app.use("/api/groups", groupRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Create HTTP server to work with Socket.io
-const server = http.createServer(app);
+// const server = http.createServer(app);
 // Socket.io
-init(server);
+// init(server);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Fallback for all other routes to serve the index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 // Clear blocked IPs older than 24 hours
 cron.schedule("0 0 * * *", async () => {
@@ -76,9 +76,9 @@ cron.schedule("0 0 * * *", async () => {
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+  console.log(`BackedApp running on port ${process.env.PORT}`)
 );
 
-server.listen( 3000, () =>
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
-);
+// server.listen(4000, () =>
+//   console.log(`WebSocketServer running on port 4000`)
+// );
