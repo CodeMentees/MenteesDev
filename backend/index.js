8da9be4dff2a0672e408411d92ff26d6dfe08aa5
ranakartikchauhan <kartikchauhan336@gmail.com>
2025-02-14 00:35:53 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -64,9 +64,9 @@ init(server);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Fallback for all other routes to serve the index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 // Clear blocked IPs older than 24 hours
 cron.schedule("0 0 * * *", async () => {
@@ -75,10 +75,12 @@ cron.schedule("0 0 * * *", async () => {
   console.log("Cleared old blocked IPs.");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+// app.listen(process.env.PORT, () =>
+//   console.log(`Server running on port ${process.env.PORT}`)
+// );
 
-server.listen( 3000, () =>
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
-);
+// server.listen( 3000, () =>
+//   console.log(`Server running on port ${process.env.PORT || 3000}`)
+// );
+
+export default app;
