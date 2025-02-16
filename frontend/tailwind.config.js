4ec151136d418fe9d29339c8ed4969cda6905cdb
ranakartikchauhan <kartikchauhan336@gmail.com>
2025-02-16 15:26:56 +0530

    fixed dark  mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
