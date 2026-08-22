d0003905551d49393d4ba5362a56912f3ca994aa
leviackerman007 <kgs.tushar522@gmail.com>
2026-08-22 15:49:55 +0000

    chore: include frontend/dist for Vercel serverless functions

---
+++ frontend/dist/assets/aos-Cqk_szRE.js
@@ -0,0 +1,1 @@
+import{g as r}from"./vendor-DyouE7zY.js";import{r as o}from"./ui-DfFTWEPM.js";var s=o();const e=r(s);export{e as A};
---
+++ frontend/dist/assets/authApi-DfFYIFAR.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useCRUD-_avfl9rD.js";const o="/auth",u=()=>{const{customRequest:t}=e(o);return{loginUser:s=>t("post","/login",s),registerUser:s=>t("post","/register",s)}};export{u};
