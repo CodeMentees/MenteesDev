5e4ff2733aa672e813204b5a5501da296cceec25
leviackerman007 <kgs.tushar522@gmail.com>
2026-08-29 19:30:21 +0000

    Edit override access was not showing

--- frontend/src/Pages/Student/MyCourses.jsx
+++ frontend/src/Pages/Student/MyCourses.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../../api/axios";
+import api from "../../api/api.js";
 import LoadingSpinner from "../../Components/UI/LoadingSpinner";
 
 function MyCourses() {
